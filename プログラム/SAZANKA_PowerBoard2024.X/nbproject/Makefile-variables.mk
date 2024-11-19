2fe30d01e8ccecd4a9d8d11536c2e8faebc10615
KIMURA TOMOHIRO <85008581+KimuraTomohiro@users.noreply.github.com>
2024-11-19 16:57:02 +0900

    ビルド

--- プログラム/SAZANKA_PowerBoard2024.X/nbproject/Makefile-variables.mk
+++ プログラム/SAZANKA_PowerBoard2024.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=SAZANKA_PowerBoard2024_2.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/SAZANKA_PowerBoard2024_2.X.production.hex
+CND_ARTIFACT_NAME_default=SAZANKA_PowerBoard2024.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SAZANKA_PowerBoard2024.X.production.hex
